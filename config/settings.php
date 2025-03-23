1aa4318908aef999219059052f56b55b63aeab46
Foxx Rivka Pinkerton <63935304+TheForestSystem@users.noreply.github.com>
2025-03-23 10:56:14 -0500

    Proper defaults

--- config/settings.php
+++ config/settings.php
@@ -2,13 +2,19 @@
 
 use Monolog\Level;
 
+if(!isset($_ENV['LOG_LEVEL'])) {
+    $level = 'error';
+} else {
+    $level = $_ENV['LOG_LEVEL'];
+}
+
 // Settings
 $settings = [
     'debug' => $_ENV['APP_DEBUG'] ?? false,
     'log' => [
         'name' => 'app',
         'path' => __DIR__ . '/../logs/app.log',
-        'level' => Level::fromName($_ENV['LOG_LEVEL']) ?? Level::Error,
+        'level' => Level::fromName($level),
     ],
 ];
 
